4776b78b81b665f63bded5de4b8a7d1ffda80f2b
Wellington Souza <wellingtonhdds27@gmail.com>
2026-05-17 00:38:53 -0300

    refactor: simplificando a entrada de lambda e mi

--- utils/input_helpers.py
+++ utils/input_helpers.py
@@ -7,52 +7,18 @@ def calculate_rate(times):
 
 
 def input_rate(symbol, key_prefix):
-    mode = st.radio(
-        f"Como deseja informar {symbol}?",
-        ["Direto", "A partir dos tempos"],
-        horizontal=True,
-        key=f"{key_prefix}_mode"
+    
+    value_str = st.text_input(
+        symbol,
+        placeholder=f"Ex: 3 (clientes/hora)",
+        key=f"{key_prefix}_input"
     )
 
-    if mode == "Direto":
-        value_str = st.text_input(
-            symbol,
-            placeholder=f"Ex: 3 (clientes/hora)",
-            key=f"{key_prefix}_input"
-        )
-
-        try:
-            return float(value_str) if value_str else 0
-        except:
-            st.error(f"Digite um número válido para {symbol}")
-            return 0
-
-    else:
-        count = st.number_input(
-            f"Quantidade de tempos para {symbol}",
-            min_value=1,
-            step=1,
-            key=f"{key_prefix}_count"
-        )
-
-        times = []
-        cols = st.columns(3)
-
-        for i in range(count):
-            with cols[i % 3]:
-                t_str = st.text_input(
-                    f"{symbol} - T{i+1}",
-                    placeholder="Ex: 10",
-                    key=f"{key_prefix}_T_{i}"
-                )
-
-                try:
-                    t = float(t_str) if t_str else 0
-                except:
-                    st.error(f"Valor inválido em T{i+1} ({symbol})")
-                    t = 0
-
-                times.append(t)
+    try:
+        return float(value_str) if value_str else 0
+    except:
+        st.error(f"Digite um número válido para {symbol}")
+        return 0
 
         value = calculate_rate(times)
 
