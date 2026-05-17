dca7079ba558e7baf72b0b438a61908bc144a4ad
Wellington Souza <wellingtonhdds27@gmail.com>
2026-05-17 00:40:45 -0300

    feat: adicionando as tabs de M/M/1/K e M/M/s>1/k

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import streamlit as st
-from views import mm1_page, mms_page
+from views import mm1_page, mms_page, mm1k_page, mmsk_page
 
 st.title("Simulador de Filas")
 
-tab1, tab2 = st.tabs(["M/M/1", "M/M/s>1"])
+tab1, tab2, tab3, tab4 = st.tabs(["M/M/1", "M/M/s>1", "M/M/1/K", "M/M/s>1/K"])
 
 with tab1:
     mm1_page.render()
 
 with tab2:
     mms_page.render()
+
+with tab3:
+    mm1k_page.render()
+
+with tab4:
+    mmsk_page.render()
