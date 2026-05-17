b5ac3a32976deb0f89ec8a5358744105866ec01d
Wellington Souza <wellingtonhdds27@gmail.com>
2026-05-17 13:41:06 -0300

    feat: adicionando os modelos M/M/1/N e M/M/s>1/N

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from views import mm1_page, mms_page, mm1k_page, mmsk_page
+from views import mm1_page, mms_page, mm1k_page, mmsk_page, mm1n_page, mmsn_page
 
 st.title("Simulador de Filas")
 
-tab1, tab2, tab3, tab4 = st.tabs(["M/M/1", "M/M/s>1", "M/M/1/K", "M/M/s>1/K"])
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["M/M/1", "M/M/s>1", "M/M/1/K", "M/M/s>1/K", "M/M/1/N", "M/M/s>1/N"])
 
 with tab1:
     mm1_page.render()
@@ -16,3 +16,9 @@ with tab3:
 
 with tab4:
     mmsk_page.render()
+
+with tab5:
+    mm1n_page.render()
+
+with tab6:
+    mmsn_page.render()
